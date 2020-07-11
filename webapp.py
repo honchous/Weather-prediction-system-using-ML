f79e6eeabc1bc157d0aa00d4642121639b6dabaa
honchous <65153939+honchous@users.noreply.github.com>
2020-07-11 20:37:42 +0530

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 18:35:55 2020
+
+@author: Anustup
+"""
+
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('weather1.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Predicted Weather {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
